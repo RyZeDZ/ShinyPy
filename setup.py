17113408ac1da57b6f345ae3b812d66bd638c2a6
RyZeDZ <mohamed@mohamed>
2023-11-02 20:56:35 +0100

    Turned code into package (hopefully)

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0a' 
+DESCRIPTION = 'An API wrapper for ShinyDB'
+LONG_DESCRIPTION = 'Just an API wrapper for ShinyDB server. So cool!'
+requirements = []
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+# Setting up
+setup(
+        name="ShinyDB", 
+        version=VERSION,
+        author="RyZeDZ",
+        author_email="<cuzimkamel@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=requirements,
+        keywords=['python'],
+        classifiers= [
+            "Programming Language :: Python :: 3",
+        ]
+)

6df65402c2a971f2f6d212d5a0256bad1e089df8
RyZeDZ <mohamed@mohamed>
2024-01-27 21:32:16 +0100

    Updated version to 1.0.5a

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.0.4a' 
+VERSION = '1.0.5a' 
 DESCRIPTION = 'An API wrapper for ShinyDB'
 LONG_DESCRIPTION = 'Just an API wrapper for ShinyDB server. So cool!'
 requirements = []
